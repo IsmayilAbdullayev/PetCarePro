8efea8d16c2b34038fb3362752cb4e70f2a0ffc0
IsmayilAbdullayev <ismayilmasalli5@gmail.com>
2017-06-15 11:55:15 -0700

    Front-end

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items:1,
+      nav:true,
+      loop:true,
+      autoplay:true,
+      navText:[
+       "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+      ]
+
+  });
+});
